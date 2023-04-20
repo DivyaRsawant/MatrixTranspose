251a7504eee99ab565b76b4762dd48a277499b8b
Divya Sawant <86359301+DivyaRsawant@users.noreply.github.com>
2023-04-20 21:14:21 +0530

    Add files via upload

---
+++ MatrixTranspose.py
@@ -0,0 +1,15 @@
+# transpose (eg: 2*3)a matrix using a nested loop
+
+X = [[12,7],
+    [4 ,5],
+    [3 ,8]]
+
+result = [[0,0,0],
+         [0,0,0]]
+
+for i in range(len(X)):
+   for j in range(len(X[0])):
+       result[j][i] = X[i][j]
+
+for r in result:
+   print(r)
